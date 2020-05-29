770bdee774d5c8c410e82e48fbb9fd470e382e88
Speedy6451 <37423245+Speedy6451@users.noreply.github.com>
2020-05-28 21:54:56 -0500

    3 lines. classes r cool

--- balance.py
+++ balance.py
@@ -1,52 +1,5 @@
 #!/usr/bin/env python3
-import time
-from tools import Robot, PID
-import tools
+from tools import Balance
 
-# Robot
-r = Robot()
-
-# PIDs
-p = PID(1.8,.6,.20)
-
-# Constants
-balance = 46 # gyro ange of balance
-tipMax = 25 # max above target before quit
-tipMin = 45 # max below target before quit
-
-# Vars
-target = 0
-error = 0
-upright = False
-lastTime = time.time()
-
-# Functions
-def resetGyro(): # resets the gyro. use when tobot is lying down on a flat surface.
-    global target, lastTime
-    target = r.g.value() - balance
-    lastTime = time.time()
-
-def isUpright(angle): # Calculates if the robot will be able to right itself,
-    global upright
-    if (angle > balance - tipMin and angle < balance + tipMax):
-        upright = True
-        return True
-    upright = False
-    return False
-
-def balanceLoop(angle, rotate): # call every loop to balance
-    gyro = r.g.value()
-    error = (target - gyro) + angle
-    out = p.loop(error)
-    r.drive(out, rotate)
-
-def hold(): # Balances upright until end of time
-    resetGyro()
-    while True:
-        balanceLoop(0,0)
-
-if __name__ == '__main__':
-    try:
-        hold()
-    finally:
-        r.stop()
+b = Balance()
+b.hold()
--- tools.py
+++ tools.py
@@ -1,26 +1,27 @@
-#!/usr/bin/env python3 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
+#!/usr/bin/env python3
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_3
 from ev3dev2.sensor.lego import TouchSensor, GyroSensor
 from ev3dev2.led import Leds
 import time
 
-class Robot:
+class Robot: # Hardware class
     def __init__(self):
         # Motors
         self.m1 = LargeMotor(OUTPUT_B)
         self.m2 = LargeMotor(OUTPUT_C)
-
         # Sensors
         self.g = GyroSensor(INPUT_3)
+
     def drive(self, go, turn): # drive with the specified forward and steer values
         self.m1.on(SpeedPercent(clampa(go + turn, 100)))
         self.m2.on(SpeedPercent(clampa(go - turn, 100)))
+
     def stop(self): # self documenting
         self.m1.stop()
         self.m2.stop()
 
-class PID:
+class PID: # PID class
     def __init__(self,kp,ki,kd,k1=1,k2=0):
         self.kp = kp # proportional gain
         self.ki = ki # integral gain
@@ -29,18 +30,17 @@ class PID:
         self.le = 0  # last error
         self.k1 = k1 # 1st input gain
         self.k2 = k2 # 2nd input gain
-        self.lt = time.time() # last time
+        self.t = Dt() # delta time
+
     def loop(self,e1,e2=0,d=False):
         e = self.k1 * e1 + self.k2 * e2 
-        t = time.time()
-        dt = t - self.lt # delta time
-        self.i = self.i + (e * dt) # integral
-        d = (e - self.le)/dt
+        self.t.update() # update delta time
+        self.i = self.i + (e * self.t.dt) # integral
+        d = (e - self.le)/self.t.dt
         o = (e * self.kp) + (self.i * self.ki) + (d * self.kd) # output
         if (not(e > 0 and self.le > 0)):
             self.i = 0
         self.le = e # the past is now
-        self.lt = t
         if d: # debug
             print("""
             dt: {}
@@ -48,9 +48,56 @@ class PID:
             p: {}
             d: {}
             o: {}
-            """.format(dt,self.i,e,d,o))
+            """.format(self.t.dt,self.i,e,d,o))
         return o
-        
+
+class Dt: # Delta time class
+    def __init__(self):
+        self.time = time.time()
+        self.lastTime = self.time
+        self.dt = 0
+
+    def update(self): # Find time since last update
+        self.time = time.time()
+        self.dt = self.lastTime - self.time
+        self.lastTime = self.time
+        return self.dt
+
+class Balance:
+    def __init__(self, pid=PID(1.8,.6,.20), robot=Robot(),  balance=46):
+        self.pid = pid
+        self.r = robot
+        self.balance = balance
+        self.tipMax = 25
+        self.tipMin = 45
+        self.target = 0 
+        self.error = 0
+        self.upright = False
+        self.resetGyro()
+
+    def resetGyro(self):
+        self.target = self.r.g.value() - self.balance
+
+    def isUpright(self, angle):
+        if (angle > self.balance - self.tipMin and angle < self.balance + self.tipMax):
+            self.upright = True
+            return True
+        self.upright = False
+        return False
+
+    def balanceLoop(self, angle, rotate): # call every loop to balance
+        gyro = self.r.g.value()
+        error = (self.target - gyro) + angle
+        out = self.pid.loop(error)
+        self.r.drive(out, rotate)
+
+    def hold(self): # Balances upright until end of time
+        self.resetGyro()
+        try:
+            while True:
+                self.balanceLoop(0,0)
+        finally:
+            self.r.stop()
 def clamp(num, minn, maxn): # constrains a number between two others
     return max(min(maxn, num), minn)
 
