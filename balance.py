fcfeb15132d7380b21f5b52deb6895276e756de9
Speedy6451 <37423245+Speedy6451@users.noreply.github.com>
2020-05-30 13:24:46 -0500

    fixed comile errs, but tuning is not on point.

--- balance.py
+++ balance.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from tools import Balance, PID
 
-b = Balance(PID(2.2,0,1,1,.2),54) 
-b.r.resetGyro()
+b = Balance(PID(2.2,0,1,1,.08),54) 
+b.resetGyro()
 try:
     while True:
-        b.r.balanceLoop(0,0)
+        b.balanceLoop(0,0)
 finally:
     b.r.stop()
